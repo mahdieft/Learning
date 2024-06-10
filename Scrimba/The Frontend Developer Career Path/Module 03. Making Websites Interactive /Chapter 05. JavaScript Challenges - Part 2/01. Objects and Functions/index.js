b51f0873921bb924e925596648430156d30d84c3
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2024-06-09 23:01:13 -0400

    Scrimba - The Frontend Developer Career Path - Module 03. Making Websites Interactive - Chapter 05. JavaScript Challenges - Part 2

---
+++ Scrimba/The Frontend Developer Career Path/Module 03. Making Websites Interactive /Chapter 05. JavaScript Challenges - Part 2/01. Objects and Functions/index.js
@@ -0,0 +1,11 @@
+const Person = {
+    name: 'Mahdi',
+    age: 45,
+    country: 'Canada',
+};
+
+function logData() {
+    console.log(`${Person.name} is ${Person.age} years old and lives in ${Person.country}.`);
+}
+
+logData();
---
+++ Scrimba/The Frontend Developer Career Path/Module 03. Making Websites Interactive /Chapter 05. JavaScript Challenges - Part 2/05. Logical Operators/index.js
@@ -0,0 +1,6 @@
+let dayOfMonth = 13;
+let weekday = 'Tuesday';
+
+if (dayOfMonth === 13 && weekday === 'Friday') {
+    console.log('😱');
+}
