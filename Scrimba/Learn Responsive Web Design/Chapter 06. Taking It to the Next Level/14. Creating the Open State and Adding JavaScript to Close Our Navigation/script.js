8e5037b388e3ba64584e780eebc2f667c540bff7
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2024-06-17 21:52:29 -0400

    Scrimba - Learn Responsive Web Design - Chapter 06. Taking It to the Next Level

---
+++ Scrimba/Learn Responsive Web Design/Chapter 06. Taking It to the Next Level/14. Creating the Open State and Adding JavaScript to Close Our Navigation/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const closeButton = document.querySelector('.close-nav');
+const nav = document.querySelector('.nav');
+
+closeButton.addEventListener('click', () => {
+    nav.classList.remove('navigation-open');
+});
